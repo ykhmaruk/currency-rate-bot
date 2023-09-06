241d852f36c50007d97c3aa9f89f1e3cb6dfd0b6
Yaroslav Khmaruk <yarik9656422@gmail.com>
2023-09-06 14:30:49 +0300

    created CurrencyRateMapper with Mapstruct

--- src/main/java/com/example/currency_rate_bot/mapper/CurrencyRateMapper.java
+++ src/main/java/com/example/currency_rate_bot/mapper/CurrencyRateMapper.java
@@ -1,13 +1,11 @@
 package com.example.currency_rate_bot.mapper;
 
+import com.example.currency_rate_bot.config.MapperConfig;
 import com.example.currency_rate_bot.dto.ApiCurrencyDto;
 import com.example.currency_rate_bot.model.CurrencyRate;
+import org.mapstruct.Mapper;
 
-public class CurrencyRateMapper {
-    public CurrencyRate mapToEntity(ApiCurrencyDto dto) {
-        CurrencyRate rate = new CurrencyRate();
-        rate.setRate(dto.getRate());
-        rate.setCurrencyName(dto.getCurrencyName());
-        return rate;
-    }
+@Mapper(config = MapperConfig.class)
+public interface CurrencyRateMapper {
+    CurrencyRate mapToModel(ApiCurrencyDto dto);
 }
