60bd2007ea4203198a541aca5de3038d386ea08c
Yaroslav Khmaruk <yarik9656422@gmail.com>
2023-09-07 14:03:17 +0300

    added @Scheduled

--- src/main/java/com/example/currency_rate_bot/CurrencyRateBotApplication.java
+++ src/main/java/com/example/currency_rate_bot/CurrencyRateBotApplication.java
@@ -2,7 +2,9 @@ package com.example.currency_rate_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class CurrencyRateBotApplication {
     public static void main(String[] args) {
