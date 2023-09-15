48f7945abb977774c5c5fde7f87fc55bd46ba314
Yaroslav Khmaruk <yarik9656422@gmail.com>
2023-09-15 17:27:44 +0300

    add docker/docker-compose files

--- src/main/java/com/example/currency_rate_bot/CurrencyRateBotApplication.java
+++ src/main/java/com/example/currency_rate_bot/CurrencyRateBotApplication.java
@@ -1,14 +1,19 @@
 package com.example.currency_rate_bot;
 
+import com.example.currency_rate_bot.service.CurrencyRateServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@RequiredArgsConstructor
 public class CurrencyRateBotApplication {
+    static CurrencyRateServiceImpl service;
 
     public static void main(String[] args) {
         SpringApplication.run(CurrencyRateBotApplication.class, args);
+        service.syncCurrencyRates();
     }
 }
